bdb729e87963d62e937813f43729c67837f16b6f
Kareem100 <Kareem100.github.com>
2021-02-24 04:57:26 -0500

    Defined Data Object and Query

--- app/src/main/java/com/khegazy/parstagram/ParseApplication.java
+++ app/src/main/java/com/khegazy/parstagram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.khegazy.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("F1JCZXy2W1We4WWGIZA5LkKbU4sJYOK9qGXueTPZ")
---
+++ app/src/main/java/com/khegazy/parstagram/Post.java
@@ -0,0 +1,38 @@
+package com.khegazy.parstagram;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+
+    public static final String Key_DESCRIPTION = "Description";
+    public static final String Key_Image = "Image";
+    public static final String Key_USER = "User";
+
+    public String getDescription(){
+        return getString(Key_DESCRIPTION);
+    }
+
+    public void setDescription(String description){
+        put(Key_DESCRIPTION, description);
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(Key_Image);
+    }
+
+    public void setImage(ParseFile parseFile) {
+        put(Key_Image, parseFile);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser(Key_USER);
+    }
+
+    public void setUser(ParseFile user) {
+        put(Key_USER, user);
+    }
+}
